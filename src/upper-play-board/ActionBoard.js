87cf702e02a9c913fb6becf8ca66e7f90f7d488c
ianlauyin <ianlauyin@gmail.com>
2023-11-01 20:51:28 +0800

    Action selection completed

--- src/upper-play-board/ActionBoard.js
+++ src/upper-play-board/ActionBoard.js
@@ -1,14 +1,67 @@
 import React from "react";
 
 export default class ActionBoard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { selected: "" };
+  }
+
+  handleClick = (e) => {
+    console.log(this.state.selected);
+    if (this.state.selected !== "") {
+      this.setState({ selected: "" });
+      return;
+    }
+    let select = e.target.value;
+    this.setState({ selected: select });
+  };
+
   render() {
+    const { selected } = this.state;
+    console.log(selected);
     return (
       <div className="action-board">
         Action:
-        <div className="action-icon"></div>
-        <div className="action-icon"></div>
-        <div className="action-icon"></div>
-        <div className="action-icon"></div>
+        <button
+          className={selected === "1" ? "selected-icon" : "action-icon"}
+          value="1"
+          onClick={
+            selected === "1" || selected === "" ? this.handleClick : null
+          }
+        >
+          <img src={require("./ActionIcon/Action1.jpg")} alt="Action1" />
+          Move
+        </button>
+        <button
+          className={selected === "2" ? "selected-icon" : "action-icon"}
+          value="2"
+          onClick={
+            selected === "2" || selected === "" ? this.handleClick : null
+          }
+        >
+          <img src={require("./ActionIcon/Action2.jpg")} alt="Action2" />
+          Draw
+        </button>
+        <button
+          className={selected === "3" ? "selected-icon" : "action-icon"}
+          value="3"
+          onClick={
+            selected === "3" || selected === "" ? this.handleClick : null
+          }
+        >
+          <img src={require("./ActionIcon/Action3.jpg")} alt="Action3" />
+          Move
+        </button>
+        <button
+          className={selected === "4" ? "selected-icon" : "action-icon"}
+          value="4"
+          onClick={
+            selected === "4" || selected === "" ? this.handleClick : null
+          }
+        >
+          <img src={require("./ActionIcon/Action4.jpg")} alt="Action4" />
+          Move
+        </button>
         <div className="energy-icon">
           Energy:
           <br />7
