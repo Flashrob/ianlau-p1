18d868471767225d0f47efbbb77ee67369d3d828
ianlauyin <ianlauyin@gmail.com>
2023-11-02 23:32:01 +0800

    Action 1-3 Completed

--- src/check/checkAction.js
+++ src/check/checkAction.js
@@ -19,6 +19,24 @@ export function checkAction1(locationInfo, selectBullet) {
   return availableSpace;
 }
 
-export function checkAction2(locationInfo, selectBullet) {}
+export function checkAction2(locationInfo, selectBullet) {
+  const availableSpace = [];
+  let row = selectBullet.slice(-1);
+  let column = selectBullet.slice(0, -1);
+  for (let i = Number(row) + 1; i < 6; i++) {
+    if (locationInfo[column + i] === "") {
+      availableSpace.push(column + i);
+    }
+  }
 
-export function checkAction3(locationInfo, selectBullet) {}
+  return availableSpace;
+}
+
+export function checkAction3(locationInfo, selectBullet) {
+  let row = selectBullet.slice(-1);
+  let column = selectBullet.slice(0, -1);
+  if (locationInfo[column + (Number(row) - 1)] === "") {
+    return [column + (Number(row) - 1)];
+  }
+  return [];
+}
