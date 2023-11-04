16684478ecc245829ff7247b94700427114839a5
ianlauyin <ianlauyin@gmail.com>
2023-11-04 12:21:22 +0800

    Pattern 2 checking and Performing Completed

--- src/gameLogic/check/checkPattern.js
+++ src/gameLogic/check/checkPattern.js
@@ -37,7 +37,24 @@ export function checkPattern1(locationInfo) {
   return availableSpace;
 }
 
-export function checkPattern2(locationInfo) {}
+export function checkPattern2(locationInfo) {
+  const availableSpace = [];
+  const color = ["red", "blue", "green", "yellow", "pink"];
+  for (let c = 0; c <= 3; c++) {
+    for (let i = 0; i <= 4; i++) {
+      if (
+        locationInfo[color[c] + i] &&
+        locationInfo[color[c] + (i + 1)].color === "red" &&
+        (locationInfo[color[c + 1] + (i - 1)] ||
+          locationInfo[color[c + 1] + i] ||
+          locationInfo[color[c + 1] + (i + 1)])
+      ) {
+        availableSpace.push(color[c] + i);
+      }
+    }
+  }
+  return availableSpace;
+}
 export function checkPattern3(locationInfo) {}
 export function checkPattern4(locationInfo) {}
 export function checkPattern5(locationInfo) {}
--- src/gameLogic/perform/performPattern.js
+++ src/gameLogic/perform/performPattern.js
@@ -21,18 +21,35 @@ export function performPattern1(locationInfo, selectPlace) {
   let column = selectPlace.slice(0, -1);
   let columnIndex = column.indexOf(column);
   let row = selectPlace.slice(-1);
-  for (let i = columnIndex - 1; i <= columnIndex + 1; i++) {
-    let target = locationInfo[color[i] + (Number(row) + 1)];
+  for (let c = columnIndex - 1; c <= columnIndex + 1; c++) {
+    let target = locationInfo[color[c] + (Number(row) + 1)];
     if (target) {
       target.star && (energyGain += 1);
       erased += 1;
-      locationInfo[color[i] + (Number(row) + 1)] = "";
+      locationInfo[color[c] + (Number(row) + 1)] = "";
     }
   }
 
   return { erased, energyGain, locationInfo };
 }
-export function performPattern2(locationInfo, selectPlace) {}
+export function performPattern2(locationInfo, selectPlace) {
+  let erased = 0,
+    energyGain = 0;
+  const color = ["red", "blue", "green", "yellow", "pink"];
+  let column = selectPlace.slice(0, -1);
+  let c = column.indexOf(column);
+  let row = selectPlace.slice(-1);
+  for (let i = Number(row) - 1; i <= Number(row) + 1; i++) {
+    let target = locationInfo[color[c + 1] + i];
+    if (target) {
+      target.star && (energyGain += 1);
+      erased += 1;
+      locationInfo[color[c + 1] + i] = "";
+    }
+  }
+
+  return { erased, energyGain, locationInfo };
+}
 export function performPattern3(locationInfo, selectPlace) {}
 export function performPattern4(locationInfo, selectPlace) {}
 export function performPattern5(locationInfo, selectPlace) {}
