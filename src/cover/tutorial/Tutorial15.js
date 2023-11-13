0dfd2d6a027921454180f067b87455ae69a90b1f
ianlauyin <ianlauyin@gmail.com>
2023-11-13 22:43:41 +0800

    Completed

--- src/cover/tutorial/Tutorial15.js
+++ src/cover/tutorial/Tutorial15.js
@@ -1,11 +1,11 @@
 export default function Tutorial15() {
   return (
     <div className="tutorial15">
+      <h1>↑</h1>
       <h5>
         When there is no available space to place the bullet, the HP will be
         reduced.
       </h5>
-      <h1>↓</h1>
     </div>
   );
 }
